43112c82773fba6966e4e97deb01550b89612377
Chris <suruceanu.cristian@gmail.com>
2022-11-15 21:59:25 +0200

    Project initialization

---
+++ src/main/kotlin/itd/usm/backend/ItdUsmBackendApplication.kt
@@ -0,0 +1,11 @@
+package itd.usm.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItdUsmBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItdUsmBackendApplication>(*args)
+}
---
+++ src/test/kotlin/itd/usm/backend/ItdUsmBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package itd.usm.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItdUsmBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
